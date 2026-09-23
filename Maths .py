b0fe4c80d90b99ecb503b51917f9f59c1a627c8e
heeral17 <jain1heeral@gmail.com>
2026-09-23 23:35:41 +0530

    added ques

--- Maths .py
+++ Maths .py
@@ -1,91 +1,147 @@
-'''
-class node:
-    def __init__(self,info,next=None):
-        self.data=info
-        self.next=next
-        
-class singlell:
-    def __init__(self,head=None):
-        self.head=head
-     
-    def inEnd(self,value):
-        temp=node(value)
-        if self.head!=None:
-            t1=self.head
-            while temp.next!=None:
-                t=t1.next
-            t1.next=temp
-        else:
-            self.head=temp
-            
-    
-    def Beg(self,value):
-        temp=node(value)
-        temp.next=self.head
-        self.head=temp
-        
-    def mid(self,value,x):
-        temp=node(value)
-        t1=self.head
-        while (t1.next!=None):
-            if t1.data==x:
-                temp.next=t1.next
-                t1.next=temp
-            else:
-                t1=t1.next        
-            
-    def pr(self):
-        t1=self.head
-        while t1.next!=None:
-            print(t1.data)
-            t1=t1.next                           
-        print(t1.data)        
-                
-ob=singlell()
-ob.inEnd(30)
-ob.inEnd(10)
-ob.mid(25,30)
-ob.beg(6)
-ob.pr()
-                
-                
-
-
-
-##1.) Extraction of Digits 
-
-n=int(input("Enter number  :"))
-while n>0:
-    last_digit=n%10
-    print(last_digit)
-    n=n//10
-
-    
-## 2.)Count Digit 
-n=int(input("Enter no "))
-count=0
-while n>0:
-    last_digit=n%10
-    count=count+1
-    n=n//10
-print(count)    
+#Docstring for Maths
+from math import sqrt
 
 '''
+1 Extarction of digits 
 
-## 3 ) Palindrome 
-def pali(n):
-    original=n
-    rev_no=0
+def ext(n):
     while n>0:
-        last_digit=n%10 
-        rev_no=rev_no*10+last_digit
+        e=n%10
+        print(e,end=" ")
+        n=n//10
+n=int(input("Enter a no: "))
+ext(n)
+
+
+#2 Count Digits
+def count(n):
+    ct=0
+    while n>0:
+        c=n%10
+        ct+=1
+        n=n//10
+    print(ct)
+n=int(input("enter a no: "))
+count(n)        
+
+
+
+#3.Armstrong no
+def arms(n):
+    org=n
+    sum=0
+    digit=len(str(org))
+    while n>0:
+        k=n%10
+        sum=sum+(k**(digit))
         n=n//10
 
-    if original==rev_no:
-        print("palindrome")
+    if org==sum:
+        print("It is Armstrong!!")
+    elif n<0:
+        print("NT for negative no")
+    else:
+        print("NOT") 
+n=int(input("ENter a no."))
+arms(n)
+
+
+#4 Reverse no
+
+def rev(n):
+    rev=0
+    while n>0:
+        k=n%10
+        rev=(10*rev)+k
+        n=n//10
+
+    print(f"Reversed no is :{rev}" )
+
+n=int(input("Enter the original no"))   
+rev(n)     
+
+
+#5 Palindrome 
+def pali(n):
+    org=n
+    rev=0
+    while n>0:
+        k=n%10
+        rev=(10*rev)+k
+        n=n//10
+
+    if org==rev:
+        print("Palindrome !") 
+    else:
+        print("oops)(")
+
+n=int(input("enter the no: "))          
+pali(n)
+
+
+
+#6 Factors 
+def factors(n):
+    fact=[]
+    for i in range(1,int(sqrt(n))+1):
+        if n%i==0:
+            fact.append(i)
+            if n//i!=i:
+                fact.append(n//i)
+    fact.sort()
+    print(fact)
+n=int(input("enter the no "))            
+factors(n)
+
+
+#7 Fibonacci
+
+def fibo(n):
+    sum,a,b=0,0,1
+    for i in range(n):
+        sum=a+b
+        a,b=b,sum
+    print(sum)
+
+n=int(input("enter the NO"))        
+fibo(n)
+
+
+#8 factorial
+def fact(n):
+    multiply=1
+    for i in range(1,n+1):
+        multiply=multiply*i  
+
+    print(multiply)
+n=int(input("enter ::"))    
+fact(n)
+
+
+#9 Prime 
+
+def prime(n):
+
+    if n<2:
+        print("NOT PRIME ")
+    for i in range(2,int(sqrt(n))+1):
+        if n%i==0 :
+            print('NOT a prime')
+            return 
+    print("Its  a PRIME NO ")
+
+n=int(input("enter the no"))
+prime(n)
+
+
+'''
+#10 Perfect square
+
+def sq(n):
+    if n//sqrt(n)==sqrt(n):
+        print("perfect")
     else:
         print("not")
 
-n=int(input("Check:"))
-pali(n)
-
+n=int(input("no   pls"))
+sq(n)
